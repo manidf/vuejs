79c12858d7daff277308b46e70aaa04720c23fd4
manidf <mannuel.ferreira@gmail.com>
2016-09-30 22:42:43 +0200

    subscription plans looking good

--- 05_subscriptions/app.js
+++ 05_subscriptions/app.js
@@ -13,17 +13,18 @@ new Vue({
         active: {}
     },
 
+    computed: {
+        isUgrade: function () {
+            return this.plan.price > this.active.price;
+            // this.plan.price
+            // this.active.price
+        }
+    },
+
     components: {
         plan: {
             template: '#plan-template',
-            props: ['plan'],
-
-            data: function () {
-                return {
-                    active: false
-                };
-            },
-
+            props: ['plan', 'active'],
             methods: {
                 setActivePlan: function () { //bind to @click on button to select active plan
                     this.active = this.plan;
