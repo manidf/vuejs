990b934e46c9b7d83d738b51a9320519013f6084
manidf <mannuel.ferreira@gmail.com>
2016-09-30 19:00:14 +0200

    first commit

---
+++ helloworld/app.js
@@ -0,0 +1,17 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js'
+    }
+});
+
+new Vue({
+    el: '#todoApp',
+    data: {
+        todos: [
+            { text: 'Learn Js' },
+            { text: 'Learn Vue Js' }
+        ]
+    }
+});
