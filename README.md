0e58339f3ce6b7d25b2e71a2d932f1341c628e7f
manidf <mannuel.ferreira@gmail.com>
2016-09-30 22:58:38 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vuejs Playground
+
+Run simple python http server to get going
+```
+$ python -m SimpleHTTPServer 8000
+```
+
+> then navigation to http://localhost:8000 in the browser.
+
+You can also watch the original series here https://laracasts.com/series/learning-vue-step-by-step/
---
+++ assets/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        links: [
+            { name: '01. Hello World', path: '/01_helloworld'},
+            { name: '02. Vue Show', path: '/02_vueshow'},
+            { name: '03. Event Handling', path: '/03_eventhandling'},
+            { name: '04. Computed Properties', path: '/04_computedproperties'},
+            { name: '05. Subscriptions', path: '/05_subscriptions'}
+        ]
+    }
+});
