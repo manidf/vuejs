f77726efa5be73a2b6b3025efaaa4b433eda785e
manidf <mannuel.ferreira@gmail.com>
2016-09-30 20:34:47 +0200

    form and counter example

--- 03_eventhandling/app.js
+++ 03_eventhandling/app.js
@@ -1,7 +1,29 @@
 
 new Vue({
+    
     el: '#app', // bind to element in html
-    data: {
-        message: '' // init to an empty string as good practice
+    
+    methods: {
+        handleIt: function () { // any method needs to nested within a methods object
+            console.log('Handled');
+            alert('handled');
+        }
     }
+    
+});
+
+new Vue({
+
+    el: '#counter',
+
+    data: {
+        count: 0
+    },
+
+    methods: {
+        updateCount: function () {
+            this.count += 1;
+        }
+    }
+
 });
