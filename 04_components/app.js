d0e8a7bff72a8bf5e06d4b15e4fb1e96ec239c05
manidf <mannuel.ferreira@gmail.com>
2016-10-02 14:03:51 +0200

    components are cool

--- 04_components/app.js
+++ 04_components/app.js
@@ -1,28 +1,40 @@
 
-
+// if you need more than one instance of an object/component to have same functionality
+// always make it a vue-component with its own reusable data and props and functions.
 Vue.component('my-counter', {
+    // this is a global isntance of a vue component, if you want to make it not global add it to the root vue instance below.
     template: '#counter-template',
+
+    // pass data in from outside using properties
+    props: ['subject'],
 
     // return a function, does not work the same as a normal vue root instance.
     data: function () {
         return {
             count: 0 
         };
-    } 
+    }
 });
 
 new Vue({
-
+    // always create a 'root vue intance'
     el: '#counter',
 
-    // data: {
-    //     count: 0
-    // },
+    // to encapsulate a vue-component to a particular vue-instance with a unique ID use the components methods
+    /*
+    component: {
+        counter: {
+            template: '#counter-template',
 
-    // methods: {
-    //     updateCount: function () {
-    //         this.count += 1;
-    //     }
-    // }
+            props: ['subject'],
+
+            data: function () {
+                return {
+                    count: 0 
+                };
+            } 
+        }
+    }
+    */
 
 });
