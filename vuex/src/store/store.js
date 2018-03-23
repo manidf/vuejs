8a3982438d46b2ec164bed70ceec5d475a958ef5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-03-23 13:37:13 +0200

    created getter method in the store to allow other components to be able to call it and set the components state, based on the calculation within the doubleCounter method

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -4,7 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
+    // central store, where we store our application state
+    // can be accessed by child components
+    state: { // object to store our properties
         counter: 0
+    },
+    getters: {
+        doubleCounter: state => {
+            // a function that gets the state as an argument, now components can call this getter method.
+            return state.counter * 2
+        }
     }
-})
+});
