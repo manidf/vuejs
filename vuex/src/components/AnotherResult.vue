1eed32cb31658248831427ab9a53ed7d76d8b757
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-11 22:53:13 +0200

    wip

---
+++ vuex/src/components/AnotherResult.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks: {{ stringCounter }}</p>
+</div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapGetters([ // spread operator pull and creat seperate key value pairs out of mapped getters
+                'doubleCounter',
+                'stringCounter'
+            ])
+        }
+    }
+</script>
