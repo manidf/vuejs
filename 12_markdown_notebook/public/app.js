956d858197fd1493c761b156f9dc50449fa22cc4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-13 17:26:56 +0200

    server, add note button and functions

--- 12_markdown_notebook/public/app.js
+++ 12_markdown_notebook/public/app.js
@@ -4,23 +4,29 @@ const html = marked('***BOld** *Italic* [link](http://themwebs.me)')
 
 new Vue({
     el: '#notebook',
-    data () {
+    data() {
         return {
-            content: 'Hello world',
+            content: localStorage.getItem('content') || 'You can write in **markdown**',
             notes: [],
-            items: []
+            items: [],
+            selectedId: null
         }
     },
-    created () {
+    created() {
         // set content to the stored value
         this.content = localStorage.getItem('content') || 'You can write in **markdown**'
     },
     computed: {
-        notePreview () {
-            return marked(this.content)
+        notePreview() {
+            // Markdown rendered to HTML
+            return this.selectedNote ? marked(this.selectedNote.content) : ''
         },
-        addButtonTitle () {
+        addButtonTitle() {
             return this.notes.length + ' note(s) already'
+        },
+        selectedNote() {
+            // return the matching note with selectedId
+            return this.notes.find(note => note.id === this.selectedId)
         }
     },
     watch: {
@@ -31,26 +37,29 @@ new Vue({
         immediate: true
     },
     methods: {
-        saveNote (val) {
+        saveNote(val) {
             console.log('saving note: ', this.content)
             localStorage.setItem('content', this.content)
             this.reportOperation('saving', val)
         },
-        reportOperation (opName) {
+        reportOperation(opName) {
             console.log('The ', opName, ' operation was completed!')
         },
-        addNote () {
+        addNote() {
             const time = Date.now()
             // Default new note
             const note = {
-              id: String(time),
-              title: 'New note ' + (this.notes.length + 1),
-              content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
-              created: time,
-              favorite: false,
+                id: String(time),
+                title: 'New note ' + (this.notes.length + 1),
+                content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+                created: time,
+                favorite: false,
             }
             // Add to the list
             this.notes.push(note)
+        },
+        selectNote(note) {
+            this.selectedId = note.id
         }
     }
 })
---
+++ 12_markdown_notebook/server.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`server started on port ${port}`))
