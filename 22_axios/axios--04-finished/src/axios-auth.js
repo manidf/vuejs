f7b87ef318e4735dbed6d799fd850f53631133f4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-12-08 15:37:10 +0200

    remove

--- 22_axios/axios--04-finished/src/axios-auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'https://vue-update.firebaseio.com'
-})
-
-instance.defaults.headers.common['SOMETHING'] = 'something'
-
-export default instance
